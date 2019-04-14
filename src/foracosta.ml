8e31381d73008259f39afe753a9cc307fb53325c
eddie <appositum@protonmail.com>
2019-04-14 14:40:54 -0300

    `read_file` function

--- src/foracosta.ml
+++ src/foracosta.ml
@@ -47,3 +47,15 @@ let rec eval stack = function
   | Pop :: xs ->
     let _ = Stack.pop stack in
     eval stack xs
+
+let read_file filename = 
+  let open Caml in
+  let lines = ref [] in
+  let chan = open_in filename in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done; !lines
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines
